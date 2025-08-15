1889e07091db0a8a9082094dfd066a416c36372f
harichandra-github <harichandrachaudhari21@gmail.com>
2025-08-15 21:51:26 +0530

    Added a demo test class

---
+++ src/test/java/tests/Demo.java
@@ -0,0 +1,16 @@
+package tests;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+
+    @Test
+    public void sampleTest() {
+        
+    }
+}
