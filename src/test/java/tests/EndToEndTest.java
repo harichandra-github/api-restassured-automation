a60788f25bfdcedc528221985940fb7df2f3f168
harichandra-github <harichandrachaudhari21@gmail.com>
2025-08-16 12:11:56 +0530

    updated the LoginTest

--- src/test/java/tests/EndToEndTest.java
+++ src/test/java/tests/EndToEndTest.java
@@ -5,26 +5,38 @@ import models.request.LoginRequest;
 import models.response.LoginResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import services.base.AuthService;
+import utils.ConfigReader;
 
+@Listeners(listeners.TestListeners.class)
 public class EndToEndTest {
+
+
 private final Logger logger= LogManager.getLogger(EndToEndTest.class);
     private AuthService authService;
     private String authToken;
 
     @Test
-    public void sampleTest() {
+    public void loginTest() {
         authService = new AuthService();
+        String username = ConfigReader.getProperty("username");
+        String password = ConfigReader.getProperty("password");
 
 
         // Step 1: Login and Extract Token
-        LoginRequest credentials = new LoginRequest("michaelw", "michaelwpass");
+        LoginRequest credentials = new LoginRequest(username, password);
         Response loginResponse = authService.login(credentials);
         loginResponse.then().statusCode(200);
-        authToken = loginResponse.as(LoginResponse.class).getAccessToken();
+        LoginResponse logresponse = loginResponse.as(LoginResponse.class);
+        authToken = logresponse.getAccessToken();
         logger.info("Login successful, token: " + authToken);
-        System.out.println(loginResponse.asPrettyString());
+        logger.info(logresponse);
+        Assert.assertNotNull(authToken);
+        Assert.assertEquals(logresponse.getUsername(),username, "Username does not match expected value.");
+
 
     }
 }
