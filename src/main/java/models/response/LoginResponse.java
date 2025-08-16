717eb2e81df897ebb35fe7888e525760046d437b
harichandra-github <harichandrachaudhari21@gmail.com>
2025-08-16 07:33:06 +0530

    POJO class for LoginResponse

---
+++ src/main/java/models/response/LoginResponse.java
@@ -0,0 +1,23 @@
+package models.response;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private int id;
+    private String username;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String image;
+    private String accessToken;
+    private String refreshToken;
+}
