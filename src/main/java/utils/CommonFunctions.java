3a11bd493cff1741544c1b73b6432e8dd643f7d9
harichandra-github <harichandrachaudhari21@gmail.com>
2025-08-23 03:28:37 +0530

    Added a class for common functions

---
+++ src/main/java/utils/CommonFunctions.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Properties;
+
+public class CommonFunctions {
+
+    public static String getTimeStamp() {
+        return new SimpleDateFormat("dd_MMMM_yyyy_HHmmss").format(new Date());
+    }
+
+
+    public static Properties loadTestConfig() throws IOException {
+        Properties testConfig = new Properties(System.getProperties());
+
+        try (FileInputStream fileInput = new FileInputStream("src/test/resources/configuration/config.properties")) {
+            testConfig.load(fileInput);
+        }
+
+        return testConfig;
+    }
+}
